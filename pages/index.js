47b3c0bf710ab8626e1b01a054031195799038da
Dan Bruder <dbruder@rapiddg.com>
2016-09-15 12:45:51 -0400

    Words

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,37 @@ import { Link } from 'react-router'
 import { prefixLink } from 'gatsby-helpers'
 import DocumentTitle from 'react-document-title'
 import { config } from 'config'
+import dan from './dan.jpg'
 
 export default class Index extends React.Component {
   render () {
     return (
       <DocumentTitle title={config.siteTitle}>
         <div>
-          <h2>
-            Hey. I'm Dan. 
-          </h2>
+          <div style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}>
+            <div style={{
+              marginRight: "20px"
+            }}>
+                <img style={{borderRadius: "50%", width: 100}} src={dan}/>
+              </div>
+              <div>
+                <h1>
+                  Hey. I'm Dan. 
+                </h1>
+              </div>
+            </div>
+            <div>
+              <h3>Here's some links:</h3>
+              <ul>
+                <li><a href="https://twitter.com/danbruder">Twitter</a></li>
+                <li><a href="https://github.com/danbruder">Github</a></li>
+                <li><a href="https://rapiddg.com">Rapid Development Group</a></li>
+              </ul>
+            </div>
         </div>
       </DocumentTitle>
     )
