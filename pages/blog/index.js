f5cdb2a383303a523f16c0fbbd2b0c6861013d3e
Dan Bruder <dbruder@rapiddg.com>
2016-09-20 15:03:41 -0400

    words

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -9,25 +9,23 @@ import access from 'safe-access'
 export default class extends React.Component {
   render () {
     const pageLinks = []
-    // Sort pages.
-    const sortedPages = sortBy(this.props.route.pages, (page) => access(page, 'data.date')
-                              ).reverse()
-                              sortedPages.forEach((page) => {
-                                if (access(page, 'file.ext') === 'md' && !include(page.path, '/404')) {
-                                  const title = access(page, 'data.title') || page.path
-                                  pageLinks.push(
-                                    <li key={page.path} style={style.post}>
-                                      <Link style={style.Link} to={prefixLink(page.path)}>
-                                        {title}
-                                      </Link>
-                                      <div style={style.date}>
-                                        {moment(page.data.date).calendar()}
-                                      </div>
-                                      <Summary body={page.data.body} />
-                                    </li>
-                                  )
-                                }
-                              })
+    const sortedPages = sortBy(this.props.route.pages, (page) => access(page, 'data.date')).reverse()
+    sortedPages.forEach((page) => {
+      if (access(page, 'file.ext') === 'md' && !include(page.path, '/404')) {
+        const title = access(page, 'data.title') || page.path
+        pageLinks.push(
+          <li key={page.path} style={style.post}>
+            <Link style={style.Link} to={prefixLink(page.path)}>
+              {title}
+            </Link>
+            <div style={style.date}>
+              {moment(page.data.date).calendar()}
+            </div>
+            <Summary body={page.data.body} />
+          </li>
+        )
+      }
+    })
     return (
       <DocumentTitle title={config.siteTitle}>
         <div>
